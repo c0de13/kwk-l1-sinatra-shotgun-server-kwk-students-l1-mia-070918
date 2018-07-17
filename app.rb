963ec35fae0d040332e48b9ab99987940d458720
c0de13 <meganmpuppy@gmail.com>
2018-07-17 15:28:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,13 @@ class App < Sinatra::Base
   get '/' do 
     "Welcome to your app!!!! "
   end
+  
+  get "/name" do
+    "Hello Murphy"
+  end   
 
+  get "/meh" do 
+    "Hello meh"
+  end
+  
 end
